3c83a79312e1609e488450a022248c025a24b0c3
abel <abel.wang@gmail.com>
2019-09-25 18:56:31 -0700

    printing input params

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,19 @@ async function run() {
   try {
     // get input value for resource group
     const resourceGroup = core.getInput('resourceGroup');
-    console.log('resourceGroup: ${resourceGroup}');
+    console.log('resourceGroup: ' + resourceGroup);
 
     //get input value for app service name
     const appService = core.getInput('appService');
-    console.log('appService: ${appService}');
+    console.log('appService: ' + appService);
 
-    // get inptu value for the type of connection string
+    // get input value for the type of connection string
     const connectionStringType = core.getInput('connectionStringType');
-    console.log('connectionStringType: ${connectionStringType}');
+    console.log('connectionStringType: ' + connectionStringType);
 
     // get input value for the connection string
     const connectionString = core.getInput('connectionString');
-    console.log('connectionString: ${connectionString}');
+    console.log('connectionString: ' + connectionString);
 
   } catch (error) {
     core.setFailed(error.message);
