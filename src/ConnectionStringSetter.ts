7e668b9353d86ad6e72670533b9823f8169ed452
abel <abel.wang@gmail.com>
2019-09-27 08:26:36 -0700

    debugging

--- src/ConnectionStringSetter.ts
+++ src/ConnectionStringSetter.ts
@@ -117,7 +117,7 @@ export class ConnectionStringSetter {
         console.log("    bash script path: " + bashScriptPath);
 
         // craft the command line call
-        let commandLineCall = "powershell -command " + `${bashScriptPath} -servicePrincipal ${servicePrincipal} -servicePrincipalTenant ${servicePrincipalTenant} -servicePrincipalSecret ${servicePrincipalSecret} -azureSubscriptionName ${azureSubscriptionName} -resourceGroup ${resourceGroup} -appService ${appService} -connectionStringType ${connectionStringType} -connectionString "${connectionString}" -connectionStringName "${connectionStringName}"`;
+        let commandLineCall = "powershell -command " + `${bashScriptPath} -servicePrincipal ${servicePrincipal} -servicePrincipalTenant ${servicePrincipalTenant} -servicePrincipalSecret ${servicePrincipalSecret} -azureSubscriptionName ${azureSubscriptionName} -resourceGroup ${resourceGroup} -appService ${appService} -connectionStringType ${connectionStringType} -connectionString \"${connectionString}\" -connectionStringName "${connectionStringName}"`;
         console.log("    commandLineCall: " + commandLineCall);
         console.log("");
 
